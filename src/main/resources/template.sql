b5fd1c5a77d6d87cb7d397600ebcd8693601d99a
harishkumard34 <harishkumardhanasekaran3@gmail.com>
2025-01-02 15:41:36 +0530

    cammelcase

--- src/main/resources/template.sql
+++ src/main/resources/template.sql
@@ -1,7 +1,8 @@
 CREATE TABLE email_templates (
-    templateId BIGINT AUTO_INCREMENT PRIMARY KEY,
-    templateName VARCHAR(255) NOT NULL,
+    template_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    template_name VARCHAR(255) NOT NULL,
     subject VARCHAR(255) NOT NULL,
     body VARCHAR(2000),
     properties TEXT -- Use TEXT to store larger string values
 );
+
